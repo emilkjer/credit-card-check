5eab365fd98d79d08fa58a65122fe478aece5ff9
Emil Kjer <emil@kjer.info>
2012-10-15 16:02:52 +1100

    luhn validation algorithm

--- credit_card_validator.rb
+++ credit_card_validator.rb
@@ -8,22 +8,42 @@
 class CreditCardValidator
   
   def determine_card_type(card_digits_str)
-    #34 or 37 => AMEX
-    #6011 => Discover
-    #51-55 => MasterCard
-    #4 => Visa
-    if card_digits_str =~ /^4[0-9]{12}(?:[0-9]{3})?$/
-      return "VISA"
-    elsif card_digits_str =~ /^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/ 
-      return "MasterCard"
-    elsif card_digits_str =~ /^3[47]\d{13}$/
-      return "AMEX"
-    elsif card_digits_str =~ /^6011-?\d{4}-?\d{4}-?\d{4}$/
-      return "Discover"
-    else
-      return "Unknown"
-    end
+    # Determine the credit card type based on first digits and
+    # the length of the number of digits.
+    # Determined by the following patterns:
+    # start with 34 or 37, length 15 => AMEX
+    # start with 6011, length 16 => Discover
+    # start with 51-55, length 16 => MasterCard
+    # start with 4, length 13 or 16 => Visa
     
+    if card_digits_str =~ /^4[0-9]{12}(?:[0-9]{3})?$/
+      "VISA"
+    elsif card_digits_str =~ /^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/ 
+      "MasterCard"
+    elsif card_digits_str =~ /^3[47]\d{13}$/
+      "AMEX"
+    elsif card_digits_str =~ /^6011-?\d{4}-?\d{4}-?\d{4}$/
+      "Discover"
+    else
+      "Unknown"
+    end
   end
+  
+  def validate_card_luhn(card_digits_str)
+    # Validate card digits with the Luhn algorithm 
+    # Ref: http://en.wikipedia.org/wiki/Luhn_algorithm
+    sum = 0
+    card_digits_str.reverse.split("").each_with_index do |i,idx|
+      if idx % 2 != 0
+        sub = i.to_i*2
+        sub -= 9 if sub > 9
+        sum += sub 
+      else
+        sum += i.to_i
+      end
+    end
+    sum % 10 == 0
+  end
+  
   
 end
--- spec/credit_card_validator_spec.rb
+++ spec/credit_card_validator_spec.rb
@@ -10,6 +10,15 @@ require 'spec_helper'
 describe CreditCardValidator do
   before :each do
     @credit_card_validator = CreditCardValidator.new
+    @card_digits_valid = [ "4111111111111111",
+                            "4012888888881881",
+                            "378282246310005",
+                            "6011111111111117",
+                            "5105105105105100"]
+                            
+    @card_digits_invalid = [  "4111111111111",
+                              "5105105105105106",
+                              "9111111111111111"]
   end
   
   describe "#determine_card_type" do
@@ -35,19 +44,40 @@ describe CreditCardValidator do
     
     it "should return MasterCard 5105105105105100" do
       card_digits = "5105105105105100"
-      @credit_card_validator.determine_card_type(card_digits).should eq('MasterCard') 
+      @credit_card_validator.determine_card_type(card_digits).should eq('MasterCard')
+      card_digits = "5505105105105100"
+      @credit_card_validator.determine_card_type(card_digits).should eq('MasterCard')
     end
 
-    it "should return MasterCard 5505105105105100" do
-      card_digits = "5505105105105100"
-      @credit_card_validator.determine_card_type(card_digits).should eq('MasterCard') 
-    end
 
     it "should return Unknown 5605105105105100" do
       card_digits = "5605105105105100"
       @credit_card_validator.determine_card_type(card_digits).should eq('Unknown') 
+      #Too short
+      card_digits = "550510510510510"
+      @credit_card_validator.determine_card_type(card_digits).should eq('Unknown') 
+      #Too long
+      card_digits = "55051051051051000"
+      @credit_card_validator.determine_card_type(card_digits).should eq('Unknown')
+
     end
         
+  end
+  
+  describe "#validate_card_luhn" do
+    it "should validate TRUE" do
+      @card_digits_valid.each do |card|
+        @credit_card_validator.validate_card_luhn(card).should be true
+      end
+    end
+
+    
+    it "should validate FALSE" do
+      
+      @card_digits_invalid.each do |card|
+        @credit_card_validator.validate_card_luhn(card).should be false
+      end
+    end
   end
   
   
