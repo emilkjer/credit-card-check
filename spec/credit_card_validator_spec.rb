347cf8feaccdfff83e200ce565328f922fa8204f
Emil Kjer <emil@kjer.info>
2012-10-15 17:54:12 +1100

    code cleanup

--- spec/credit_card_validator_spec.rb
+++ spec/credit_card_validator_spec.rb
@@ -4,8 +4,8 @@
   * @Author: Emil Kjer
   * Date: 15/10/2012
 =end
-require 'spec_helper'
 
+require 'spec_helper'
 
 describe CreditCardValidator do
   before :each do
@@ -22,26 +22,26 @@ describe CreditCardValidator do
   end
   
   describe "#determine_card_type" do
-    it "should return VISA 4408041234567893" do
+    it "should determine VISA" do
+      # Begin with 4
+      # Determine 16 digits
       card_digits = '4408041234567893'
+      @credit_card_validator.determine_card_type(card_digits).should eq('VISA')
+      # Determine 13 digits
+      card_digits = '4408041234567'
       @credit_card_validator.determine_card_type(card_digits).should eq('VISA')
       #Too short
       @credit_card_validator.determine_card_type(card_digits[0..-2]).should eq('Unknown') 
       #Too long
       @credit_card_validator.determine_card_type(card_digits+"0").should eq('Unknown') 
     end
-    
-    it "should return AMEX 378282246310005" do
-      card_digits = "378282246310005"
-      @credit_card_validator.determine_card_type(card_digits).should eq('AMEX')
-      #Too short
-      @credit_card_validator.determine_card_type(card_digits[0..-2]).should eq('Unknown') 
-      #Too long
-      @credit_card_validator.determine_card_type(card_digits+"0").should eq('Unknown') 
-    end
 
-    it "should return AMEX 348282246310005" do
+    it "should determine AMEX" do
+      # Begin with 34
       card_digits = "348282246310005"
+      @credit_card_validator.determine_card_type(card_digits).should eq('AMEX')
+      # Begin with 37
+      card_digits = "378282246310005"
       @credit_card_validator.determine_card_type(card_digits).should eq('AMEX') 
       #Too short
       @credit_card_validator.determine_card_type(card_digits[0..-2]).should eq('Unknown') 
@@ -49,7 +49,8 @@ describe CreditCardValidator do
       @credit_card_validator.determine_card_type(card_digits+"0").should eq('Unknown')
     end
     
-    it "should return Discover 6011111111111117" do
+    it "should determine Discover" do
+      # Begin with 6011
       card_digits = "6011111111111117"
       @credit_card_validator.determine_card_type(card_digits).should eq('Discover')
       #Too short
@@ -58,11 +59,24 @@ describe CreditCardValidator do
       @credit_card_validator.determine_card_type(card_digits+"0").should eq('Unknown') 
     end
     
-    it "should return MasterCard 5105105105105100" do
+    it "should determine MasterCard" do
+      # Begin with range 51-55
       card_digits = "5105105105105100"
+      @credit_card_validator.determine_card_type(card_digits).should eq('MasterCard')
+      card_digits = "5205105105105100"
+      @credit_card_validator.determine_card_type(card_digits).should eq('MasterCard')
+      card_digits = "5305105105105100"
+      @credit_card_validator.determine_card_type(card_digits).should eq('MasterCard')
+      card_digits = "5405105105105100"
       @credit_card_validator.determine_card_type(card_digits).should eq('MasterCard')
       card_digits = "5505105105105100"
       @credit_card_validator.determine_card_type(card_digits).should eq('MasterCard')
+      card_digits = "5005105105105100"
+      @credit_card_validator.determine_card_type(card_digits).should eq('Unknown')
+      card_digits = "5605105105105100"
+      @credit_card_validator.determine_card_type(card_digits).should eq('Unknown')
+      
+      card_digits = "5105105105105100"
       #Too short
       @credit_card_validator.determine_card_type(card_digits[0..-2]).should eq('Unknown') 
       #Too long
@@ -70,11 +84,16 @@ describe CreditCardValidator do
     end
 
 
-    it "should return Unknown 5605105105105100" do
-      card_digits = "5605105105105100"
+    it "should return Unknown on 1234" do
+      card_digits = "1234"
       @credit_card_validator.determine_card_type(card_digits).should eq('Unknown') 
     end
-        
+
+    it "should return Unknown on 'abc'" do
+      card_digits = "abc"
+      @credit_card_validator.determine_card_type(card_digits).should eq('Unknown') 
+    end
+            
   end
   
   describe "#validate_card_luhn" do
